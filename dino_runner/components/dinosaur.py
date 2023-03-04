9808cd23d616a0b3788f3ba64529c515e836c47f
Miguel1777 <Miguel.Cruz@fundacion-jala.org>
2023-03-04 15:41:40 -0500

    Second class Homework

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -8,6 +8,7 @@ class Dinosaur(Sprite):
     X_POS = 80
     Y_POS = 310
     JUMP_SPEED = 8.5
+    Y_POS_DUCK = 340
     
     def __init__(self):
         self.image = RUNNING[0]
@@ -25,13 +26,22 @@ class Dinosaur(Sprite):
             self.run()
         elif self.dino_jump:
             self.jump()
+        elif self.dino_duck:
+            self.duck()
 
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
-        elif not self.dino_jump:
+            self.dino_duck = False
+        elif user_input[pygame.K_DOWN] and not self.dino_jump:
             self.dino_jump = False
-            self.dino_run = True 
+            self.dino_run = False
+            self.dino_duck = True
+        elif not (self.dino_jump or  user_input[pygame.K_DOWN]):
+            self.dino_jump = False
+            self.dino_run = True
+            self.dino_duck = False
+
         
             
         if self.step_index >= 10:       
@@ -53,6 +63,16 @@ class Dinosaur(Sprite):
             self.dino_rect.y = self.Y_POS
             self.dino_jump = False
             self.jump_speed = self.JUMP_SPEED
+
+    def duck(self):
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS_DUCK
+        self.step_index += 1
+
+
+
 
 
 
