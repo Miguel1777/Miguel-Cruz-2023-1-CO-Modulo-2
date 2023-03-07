b4572bcae0f6ff989845d6ce15f0f113160f50c9
Miguel1777 <Miguel.Cruz@fundacion-jala.org>
2023-03-06 21:48:28 -0500

    Progress of the third class

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -40,9 +40,7 @@ class Dinosaur(Sprite):
         elif not (self.dino_jump or  user_input[pygame.K_DOWN]):
             self.dino_jump = False
             self.dino_run = True
-            self.dino_duck = False
-
-        
+            self.dino_duck = False     
             
         if self.step_index >= 10:       
             self.step_index = 0
@@ -70,11 +68,6 @@ class Dinosaur(Sprite):
         self.dino_rect.x = self.X_POS
         self.dino_rect.y = self.Y_POS_DUCK
         self.step_index += 1
-
-
-
-
-
 
 
     def draw(self, screen):
